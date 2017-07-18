3df817ce4f570ffdfa36367f1a93c5cbe71061e1
areustle <areustledev@gmail.com>
2017-07-18 11:59:34 -0400

    first crack at new build script

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
-export CFLAGS='-I${PREFIX}/include -O2 -Wall --pedantic -Wno-comment -Wno-long-long -g  -ffloat-store -fPIC'
-export CXXFLAGS='-I${PREFIX}/include -O2 -Wall --pedantic -Wno-comment -Wno-long-long -g  -ffloat-store -fPIC'
-export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
+cd f2c
+sh autogen.sh
+./configure --prefix=${PREFIX}
 make
-make install prefix=${PREFIX} exec_prefix=${PREFIX} \
-		libdir=${PREFIX}/lib includedir="${PREFIX}/include/f2c" \
-		LIBDIR=${PREFIX}/lib INCDIR="${PREFIX}/include/f2c"
+make install
