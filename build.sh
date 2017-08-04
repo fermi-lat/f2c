f53853fe1ff07b7d7383d2267ceab7d31c401b14
areustle <areustledev@gmail.com>
2017-08-04 19:32:57 -0400

    cheat to place f2c_types.h

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ sh autogen.sh
 ./configure --prefix=${PREFIX} --includedir=${PREFIX}/include/f2c
 make
 make install
-cp ${PREFIX}/include/f2c/f2c_types.h ${PREFIX}/include/f2c_types.h
+cp include/f2c_types.h ${PREFIX}/include/f2c_types.h
