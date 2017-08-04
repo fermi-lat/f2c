7ac0120a0fd4825b21eaa94d02cba326b317a3fd
areustle <areustledev@gmail.com>
2017-08-04 19:58:50 -0400

    Use FSSC f2c code

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
-sh autogen.sh
-./configure --prefix=${PREFIX} --includedir=${PREFIX}/include/f2c
 make
 make install
-cp include/f2c_types.h ${PREFIX}/include/f2c_types.h
-cp include/f2c_inline.h ${PREFIX}/include/f2c_inline.h
